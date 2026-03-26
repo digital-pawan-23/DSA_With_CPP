7309ea728cae3e67ac47370f876fa5b501e6b02d
digital-pawan-23 <pawandigitalbusiness@gmail.com>
2026-03-27 01:02:20 +0530

    Create Dsa Folder

---
+++ Basic/basic.cpp
@@ -0,0 +1,17 @@
+// BoilerCodePlate
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Pawan\n" <<"Pandey" ;
+    return 0;
+}
---
+++ Variables/SumOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float A;
+    cout<<"Enter 1st Numer : ";
+    cin>>A;
+    float B;
+    cout<<"Enter 2nd Numer : ";
+    cin>>B;
+    float Sum = A+B;
+    cout<<"Final Result : " <<Sum; 
+    return 0;
+}
