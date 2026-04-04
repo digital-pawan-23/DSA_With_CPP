3a57a082ab16a4e4a98e97ae5c62f490fbcf6ec6
digital-pawan-23 <pawandigitalbusiness@gmail.com>
2026-04-04 23:06:15 +0530

    Code Arranged In Proper Way

--- Pattern_Printing/HollowDiagram.cpp
+++ Pattern_Printing/HollowDiagram.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 int main(){
 
-    int n = 4;
-
-    // top part
-
-    for(int i=1; i<=n; i++){
+int n = 4;
+// top part
+for(int i=1; i<=n; i++){
         // for spaces
         for(int j=1; j<=n-i; j++){
             cout<<" ";
@@ -14,23 +12,17 @@ int main(){
         cout<<"*";
         // for again spaces
         if(i!=1){
-
-        for(int j=1; j<=2*i-3; j++){
+            for(int j=1; j<=2*i-3; j++){
             cout<<" ";
         }
         cout<<"*";
     }
-
     cout<<endl;
 }
-
 // bottom Part
-
 for(int i=1; i<=n-1; i++){
-
-    // for spaces
-
-    for(int j=1; j<=i; j++){
+// for spaces
+for(int j=1; j<=i; j++){
         cout<<" ";
     }
     cout<<"*";
